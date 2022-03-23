0817fbecadc7b622b0ee17e72eb143a2c4681c60
Kou <kou.yang2015@gmail.com>
2022-03-22 20:23:38 -0500

    Data classes Parcelized. Misc Updates

--- app/src/main/java/ics340kyang/weatherapp/WeatherCondition.kt
+++ app/src/main/java/ics340kyang/weatherapp/WeatherCondition.kt
@@ -1,6 +1,10 @@
 package ics340kyang.weatherapp
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class WeatherCondition(
     val main: String,
     val icon: String,
-)
+) : Parcelable
